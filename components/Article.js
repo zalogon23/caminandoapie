20870e0769be6f9df7c0b9b8da657d36dd25a9e4
ElTioGonza <gonz230300@gmail.com>
2021-08-10 11:33:57 -0300

    Article COMPONENT modification. Now can set a figcaption on STRAPI and it's set as ALT on the image element

--- components/Article.js
+++ components/Article.js
@@ -19,13 +19,13 @@ function Article({ content }) {
             {el.type === "heading" && id !== 0 && <Heading as="h3" pb="4" fontSize="1.2em">{el.content}</Heading>}
             {el.type === "link" && <Link href={el.link} passHref><ChakraLink d="block" pb="4" fontWeight="bold" color="teal.400" fontSize={fontSizes.paragraph}>{el.content}<FontAwesomeIcon style={{ marginLeft: "0.5rem" }} icon={faLink} /></ChakraLink></Link>}
             {el.type === "text" && <Text color="gray.500" pb="4" fontSize={fontSizes.paragraph}>{
-              el.content.map((piece, id)=> {
+              el.content.map((piece, id) => {
                 if (piece.type === "strong") return (<Text key={`text${id}`} as="strong">{piece.text}</Text>)
                 if (piece.type === "link") return (<Link key={`text${id}`} href={piece.link} passHref><ChakraLink fontWeight="bold" color="teal.400" fontSize={fontSizes.paragraph}>{piece.text}</ChakraLink></Link>)
-                if(piece) return piece
+                if (piece) return piece
               })
             }</Text>}
-            {el.type === "image" && <Image objectPosition="center" maxH="70vh" fit="contain" w={["full", , , "50%"]} mb="4" ml={{ "lg": el.dir === "left" ? "0": "5" }} mr={{ "lg": el.dir === "left" ? "5": "0" }} float={{ "lg": el.dir }} src={el.src} alt={el.alt} />}
+            {el.type === "image" && <Image objectPosition="center" maxH="70vh" fit="contain" w={["full", , , "50%"]} mb="4" ml={{ "lg": el.dir === "left" ? "0" : "5" }} mr={{ "lg": el.dir === "left" ? "5" : "0" }} float={{ "lg": el.dir }} src={el.src} alt={el.alt} />}
           </React.Fragment>)
       }
     </Box >
@@ -33,6 +33,7 @@ function Article({ content }) {
 
   function styleContent(content) {
     let openParagraph = false
+    let openImage = false
     let dir = "left"
     const result = []
     content.split("<").forEach(el => {
@@ -40,26 +41,31 @@ function Article({ content }) {
         openParagraph = false
         result.push({ type: "heading", content: el.slice(el.indexOf(">") + 1) })
         return
-      } else if(el.indexOf("/p") === 0){
+      } else if (el.indexOf("/p") === 0) {
         openParagraph = false
         return
-      }else if (el.indexOf("/") === 0 && openParagraph) {
-        if(el.slice(el.indexOf(">") + 1)) result[result.length - 1].content.push(el.slice(el.indexOf(">") + 1))
+      } else if (el.indexOf("/") === 0 && openParagraph) {
+        if (el.slice(el.indexOf(">") + 1)) result[result.length - 1].content.push(el.slice(el.indexOf(">") + 1))
         return
       } else if (el.indexOf("strong>") === 0) {
         if (!openParagraph) return
         result[result.length - 1].content.push({ type: "strong", text: el.slice(el.indexOf(">") + 1) })
         return
       } else if (el.indexOf("a") === 0) {
-        if(openParagraph){ result[result.length - 1].content.push({ type: "link", link: el.slice(el.indexOf('href="') + 6, el.indexOf('"', el.indexOf('href="') + 6)), text: el.slice(el.indexOf(">") + 1) }); return}
+        if (openParagraph) { result[result.length - 1].content.push({ type: "link", link: el.slice(el.indexOf('href="') + 6, el.indexOf('"', el.indexOf('href="') + 6)), text: el.slice(el.indexOf(">") + 1) }); return }
         result.push({ type: "link", link: el.slice(el.indexOf('href="') + 6, el.indexOf('"', el.indexOf('href="') + 6)), content: el.slice(el.indexOf(">") + 1) })
         return
       } else if (el.indexOf("p") === 0) {
         openParagraph = true
-        if(el.slice(el.indexOf(">") + 1)) result.push({ type: "text", content: [el.slice(el.indexOf(">") + 1)] })
+        if (el.slice(el.indexOf(">") + 1)) result.push({ type: "text", content: [el.slice(el.indexOf(">") + 1)] })
+        return
+      } else if (el.indexOf("figcaption") === 0 && openImage) {
+        openImage = false
+        if (el.slice(el.indexOf(">") + 1)) result[result.length - 1].alt = el.slice(el.indexOf(">") + 1)
         return
       } else if (el.indexOf("img") === 0) {
         openParagraph = false
+        openImage = true
         result.push({ type: "image", dir, src: el.slice(el.indexOf('src="') + 5, el.indexOf('"', el.indexOf('src="') + 5)), alt: el.slice(el.indexOf('alt="') + 5, el.indexOf('"', el.indexOf('alt="') + 5)) })
         dir = dir === "left" ? "right" : "left"
         return
