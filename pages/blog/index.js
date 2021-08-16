ea8b13366102f85f5f3706a2540093bbb7ed133a
ElTioGonza <gonz230300@gmail.com>
2021-08-16 14:42:50 -0300

    This is the first MVP

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -19,7 +19,7 @@ export default function Blog({ posts }) {
       <Hero bigImage="/blog.jpg" bigAlt="Libreria con pilares enroscados, cupulas con obras de arte pintadas, y mesas de caoba con modelos a escala de la tierra" smImage="/blogsmall.jpg" smAlt="Escritorio elegante pero austero, con unos libros rojos encima, y una lampara, acompañado de una silla. En biblioteca llena de libros hasta el suelo">
         <Heading as="h2" textAlign="center" fontSize={fontSizes.heading} color="white" px="4" py="2" rounded="xl" lineHeight="1.5em" bg="blackAlpha.700" position="absolute" left="3%" right="3%" top="18%">Descubre todo lo relacionado con viajes, historia y exploración.</Heading>
       </Hero>
-      <CardsShelf wide>
+      <CardsShelf title="Posts que pueden interesarte" wide>
         {
           posts.map(post => (
             <Card cover h={["18rem"]} key={post.id} title={post.seo.title} link={`/blog/${post.id}`} to="Leer mas" aria={`Ir al post sobre ${post.title}`} content={post.shortContent} images={[post.mainImage]} large={false} />
