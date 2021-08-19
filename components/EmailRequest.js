f37c53dcaf925a079a154683db721361e4d80d32
ElTioGonza <gonz230300@gmail.com>
2021-08-19 10:55:25 -0300

    Synced Formik with STRAPI graphql for Email Requests. And improved some Email Request styling.

--- components/EmailRequest.js
+++ components/EmailRequest.js
@@ -4,11 +4,15 @@ import React, { useState } from 'react'
 import { Formik } from 'formik'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMailBulk, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { useMutation } from '@apollo/client'
+import queries from '../lib/queries'
 
 function EmailRequest() {
   const [isOpen, setIsOpen] = useState(true)
+  const [addEmailSubscriber, { data, loading, error }] = useMutation(queries.addEmailSubscriber)
+  console.log("Data: ", data, ". Loading: ", loading)
   return (
-    <>{isOpen ?
+    <>{(isOpen && !data?.createSubscriber) ?
       <Box pos="absolute" as="article" display={isOpen ? "flex" : "none"} flexDir="column" alignItems="center" rounded="md" p="2" textAlign="center" bottom="5%" left="50%" w="90%" maxW="650px" transform="translateX(-50%)" bg="black">
         <IconButton size="sm" rounded="full" alignSelf="flex-end" aria-label="Cerrar formulario de email" onClick={() => setIsOpen(false)}><FontAwesomeIcon icon={faTimes} /></IconButton>
         <Heading pt="3" pb="5" as="h3" fontSize={fontSizes.paragraph} color="white">¡Si querés estar al tanto de los últimos tips y posts!</Heading>
@@ -18,17 +22,15 @@ function EmailRequest() {
             const errors = {};
             if (!values.name) {
               errors.name = "Debes escribir un nombre"
-            } else if (!values.email) {
+            }
+            if (!values.email) {
               errors.email = "Debes escribir un email"
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
               errors.email = "Email invalido"
             } return errors
           }
-          } onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
+          } onSubmit={(values) => {
+            addEmailSubscriber({ variables: { name: values.name, email: values.email } })
           }} >{
             ({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => {
               console.log(errors)
@@ -36,14 +38,15 @@ function EmailRequest() {
                 < Box rounded="md" d="flex" w="100%" flexDir="column" p="2" as="form" bg="white" onSubmit={handleSubmit} >
                   <FormControl id="name" pb="2">
                     <FormLabel fontSize={fontSizes.small}>Tu nombre:</FormLabel>
-                    <Input errorBorderColor="red.500" isInvalid={!!errors.name} aria-label="Coloca tu nombre para suscribirte a nuestras publicaciones, es gratis" onChange={handleChange} onBlur={handleBlur} value={values.name} fontSize={fontSizes.small} name="name" type="name" />
+                    <Input errorBorderColor="red.500" isInvalid={touched.name && errors.name} aria-label="Coloca tu nombre para suscribirte a nuestras publicaciones, es gratis" onChange={handleChange} onBlur={handleBlur} value={values.name} fontSize={fontSizes.small} name="name" type="name" />
+                    <FormHelperText textAlign="end" px="2" fontSize={fontSizes.small}>{touched.name && errors.name}</FormHelperText>
                   </FormControl>
                   <FormControl id="email" pb="4">
                     <FormLabel fontSize={fontSizes.small}>Tu email:</FormLabel>
-                    <Input errorBorderColor="red.500" isInvalid={!!errors.email} aria-label="Coloca tu email" onChange={handleChange} onBlur={handleBlur} value={values.email} fontSize={fontSizes.small} name="email" type="email" />
-                    <FormHelperText fontSize={fontSizes.small}>Estarás recibiendo semanalmente lo mejor de nuestro contenido</FormHelperText>
+                    <Input errorBorderColor="red.500" isInvalid={touched.email && errors.email} aria-label="Coloca tu email" onChange={handleChange} onBlur={handleBlur} value={values.email} fontSize={fontSizes.small} name="email" type="email" />
+                    <FormHelperText textAlign="end" px="2" fontSize={fontSizes.small}>{touched.email && errors.email}</FormHelperText>
                   </FormControl>
-                  <Button aria-label="Enviar mi email" fontSize={fontSizes.small} w="full" type="submit" colorScheme="orange">Enviar</Button>
+                  <Button isLoading={loading} aria-label="Enviar mi email" fontSize={fontSizes.small} w="full" type="submit" colorScheme="orange">Enviar</Button>
                 </Box >
               )
             }
