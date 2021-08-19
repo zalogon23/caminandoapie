66fea74e20e1af202ce57bb4a6d6bc2eb9a6d6cb
ElTioGonza <gonz230300@gmail.com>
2021-08-19 10:12:47 -0300

    Added Formik to Email Request COMPONENT (but the logic isn't still applied)

--- components/EmailRequest.js
+++ components/EmailRequest.js
@@ -1,20 +1,59 @@
-import { Box, Button, FormControl, FormHelperText, FormLabel, Heading, Input } from '@chakra-ui/react'
+import { Box, Button, FormControl, FormHelperText, FormLabel, Heading, IconButton, Input } from '@chakra-ui/react'
 import fontSizes from "../lib/fontSizes"
-import React from 'react'
+import React, { useState } from 'react'
+import { Formik } from 'formik'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMailBulk, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function EmailRequest() {
+  const [isOpen, setIsOpen] = useState(true)
   return (
-    <Box pos="absolute" as="article" rounded="md" p="2" textAlign="center" bottom="5%" left="50%" w="90%" maxW="650px" transform="translateX(-50%)" bg="black">
-      <Heading pb="4" as="h3" fontSize={fontSizes.paragraph} color="white">¡Si querés estar al tanto de los últimos tips y posts!</Heading>
-      <Box rounded="md" d="flex" flexDir="column" p="2" as="form" bg="white">
-        <FormControl id="email" pb="4">
-          <FormLabel fontSize={fontSizes.paragraph}>Tu email:</FormLabel>
-          <Input aria-label="Coloca tu email para suscribirte a nuestras publicaciones, es gratis" fontSize={fontSizes.paragraph} type="email" />
-          <FormHelperText fontSize={fontSizes.paragraph}>Estarás recibiendo semanalmente lo mejor de nuestro contenido</FormHelperText>
-        </FormControl>
-        <Button aria-label="Enviar mi email" w="full" type="submit" colorScheme="orange">Enviar</Button>
+    <>{isOpen ?
+      <Box pos="absolute" as="article" display={isOpen ? "flex" : "none"} flexDir="column" alignItems="center" rounded="md" p="2" textAlign="center" bottom="5%" left="50%" w="90%" maxW="650px" transform="translateX(-50%)" bg="black">
+        <IconButton size="sm" rounded="full" alignSelf="flex-end" aria-label="Cerrar formulario de email" onClick={() => setIsOpen(false)}><FontAwesomeIcon icon={faTimes} /></IconButton>
+        <Heading pt="3" pb="5" as="h3" fontSize={fontSizes.paragraph} color="white">¡Si querés estar al tanto de los últimos tips y posts!</Heading>
+        <Formik
+          initialValues={{ name: "", email: "" }}
+          validate={values => {
+            const errors = {};
+            if (!values.name) {
+              errors.name = "Debes escribir un nombre"
+            } else if (!values.email) {
+              errors.email = "Debes escribir un email"
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+              errors.email = "Email invalido"
+            } return errors
+          }
+          } onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }} >{
+            ({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => {
+              console.log(errors)
+              return (
+                < Box rounded="md" d="flex" w="100%" flexDir="column" p="2" as="form" bg="white" onSubmit={handleSubmit} >
+                  <FormControl id="name" pb="2">
+                    <FormLabel fontSize={fontSizes.small}>Tu nombre:</FormLabel>
+                    <Input errorBorderColor="red.500" isInvalid={!!errors.name} aria-label="Coloca tu nombre para suscribirte a nuestras publicaciones, es gratis" onChange={handleChange} onBlur={handleBlur} value={values.name} fontSize={fontSizes.small} name="name" type="name" />
+                  </FormControl>
+                  <FormControl id="email" pb="4">
+                    <FormLabel fontSize={fontSizes.small}>Tu email:</FormLabel>
+                    <Input errorBorderColor="red.500" isInvalid={!!errors.email} aria-label="Coloca tu email" onChange={handleChange} onBlur={handleBlur} value={values.email} fontSize={fontSizes.small} name="email" type="email" />
+                    <FormHelperText fontSize={fontSizes.small}>Estarás recibiendo semanalmente lo mejor de nuestro contenido</FormHelperText>
+                  </FormControl>
+                  <Button aria-label="Enviar mi email" fontSize={fontSizes.small} w="full" type="submit" colorScheme="orange">Enviar</Button>
+                </Box >
+              )
+            }
+          }
+        </Formik>
       </Box>
-    </Box>
+      :
+      <IconButton position="absolute" colorScheme="orange" bottom="5%" right="5%" alignSelf="flex-end" aria-label="Abrir formulario de email" onClick={() => setIsOpen(true)}><FontAwesomeIcon icon={faMailBulk} /></IconButton>
+    }
+    </>
   )
 }
 
